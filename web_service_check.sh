3f4636af567c73f6c59edc02ea1d057338f546d2
frogofrock <47983783+frogofrock@users.noreply.github.com>
2021-11-15 14:50:54 +0400

    добавил php7.2-fpm

--- web_service_check.sh
+++ web_service_check.sh
@@ -21,4 +21,17 @@ nginxstatus=$(systemctl status nginx | grep -Eo "running|dead|failed")
 					systemctl restart nginx #Перезапускаем nginx
 					sleep 1 #Ожидаем 1 секунду, чтобы сервер точно запустился.
 					echo -en "${WHITE} ${BGGREEN} Статус Nginx после перезапуска $(systemctl status nginx | grep -Eo "running|dead|failed") ${NORMAL}\n"
+					echo $(curl -I 192.168.31.212 | grep OK) #Проверка отдает ли веб-сервер http код 200
+			fi
+# Получим статус php через systemd, запишем его в переменную
+phpfpmstatus=$(systemctl status php7.2-fpm | grep -Eo "running|dead|failed")
+			if [[ $phpfpmstatus = 'running' ]]
+				then
+					echo -en "${WHITE} ${BGGREEN} php7.2-fpm работает ${NORMAL}\n"
+				else
+					echo -en "${WHITE} ${BGRED} Статус php7.2-fpm $phpfpmstatus Пробуем перезагрузить. ${NORMAL}\n"
+					systemctl restart php7.2-fpm #Перезапуск php7.2-fpm
+					sleep 1 #Ожидаем 1 секунду, чтобы php7.2-fpm точно запустился
+
+					echo -en "${WHITE} Статус php7.2-fpm после перезапуска $(systemctl status php7.2-fpm | grep -Eo "running|dead|failed") ${NORMAL}\n"
 			fi
